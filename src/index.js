22f861c253526ec86d9c5883964339cde681a749
Kyle Jensen <kylenjensen19@gmail.com>
2019-10-31 11:08:04 -0700

    finished the webpack

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles/index.css';
+import $ from 'jquery';
+import shoppingList from './scripts/shopping-list';
+
+const main = function () {
+  shoppingList.bindEventListeners();
+  shoppingList.render();
+};
+
+$(main);
